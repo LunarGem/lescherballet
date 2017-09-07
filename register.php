31044c6a076e3274677b8d32d3959a63ef926301
LunarGem <LunarGem@users.noreply.github.com>
2017-09-07 16:16:25 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,14 @@
-//registration for classes (via Paypal or something)
+<title>Registration</title>
+<body><center>
+  <div class="container">
+  <!--header stuff-->
+    <div class="row">
+     <form class="col s12">
+      <div class="row">
+      <div class="input-field col s6">
+      
+      </div>
+      </div>
+     </form>
+  </div>
+</center></body>
