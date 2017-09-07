29f8d1155c29e8bf48f5df8e5494ac2fd4b3364b
LunarGem <LunarGem@users.noreply.github.com>
2017-09-07 15:50:31 -0400

    Add basic framework
    TODO: styles

--- index.php
+++ index.php
@@ -1,16 +1,67 @@
 <!DOCTYPE html>
 <html>
   <head>
+   
     <title>
         Les Cher Ballet
     </title>
-  </head>
+
   <body><center>
     <div class="container">
       <div class="row">
         <div class="col m8 s12 offset-m2">
           <h1>Les Cher Ballet</h1>
           <p>Your tagline here</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="card">
+            <div class="card-image">
+              <!--<img src="images/sample-1.jpg">-->
+              Your image goes here
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">About Us</a>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="card">
+            <div class="card-image">
+              <!--<img src="images/sample-1.jpg">-->
+              Your image goes here
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Classes</a>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="card">
+            <div class="card-image">
+              <!--<img src="images/sample-1.jpg">-->
+              Your Image goes here
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Showcase</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
