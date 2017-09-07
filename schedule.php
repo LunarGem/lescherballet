388be387db2cbfee8147da9f283c1c5db70fa888
LunarGem <LunarGem@users.noreply.github.com>
2017-09-07 16:09:14 -0400

    Add basic framework
    TODO: style, finish tables

--- schedule.php
+++ schedule.php
@@ -1,1 +1,67 @@
-//class schedule (summer and main term) plus cost information
+<!DOCTYPE html>
+<html>
+  <head><title>Classes</title></head>
+<body><center>
+  <div class="container">
+  <!--header stuff-->
+    <div class="row">
+    <p>
+    *stuff on regular site about art, uniforms, reqs etc*
+    </p>
+      <table class="bordered responsive-table centered">
+        <tr style="text-align: center;">
+          <th>Group</th>
+          <th>Days</th>
+          <th>Times</th>
+          <th>Teacher</th>
+        </tr>
+        <tr>
+          <td><a href="#primary">Primary (3-5 years old)</a></td>
+          <td>Mondays</td>
+          <td>5:00 pm to 6:00pm</td>
+          <td>(insert regular teacher with link to teacher profile)</td>
+        </tr>
+        <tr>
+        <td><a href="#primary">Primary (3-5 years old)</a></td>
+          <td>Mondays</td>
+          <td>5:00 pm to 6:00pm</td>
+          <td>(insert regular teacher with link to teacher profile)</td>
+        </tr>
+        <tr>
+          <td>Primary (3-5 years old)</td>
+          <td>Mondays</td>
+          <td>5:00 pm to 6:00pm</td>
+          <td>(insert regular teacher with link to teacher profile)</td>
+        </tr>
+        <tr>
+          <td>Primary (3-5 years old)</td>
+          <td>Mondays</td>
+          <td>5:00 pm to 6:00pm</td>
+          <td>(insert regular teacher with link to teacher profile)</td>
+        </tr>
+      </table>
+    </div>
+    <div class="row">
+      <p>
+      *stuff about registration, semesters, due dates, discounts etc*
+      </p>
+      <table class="bordered resposive-table centered">
+        <tr>
+          <th>Hours</th>
+          <th>Regular Price</th>
+        </tr>
+        <tr>
+          <td>45 min - 1 hr/wk</td>
+          <td>$219.00</td>
+        </tr>
+      </table>
+    </div>
+    <div class="row">
+      <p>
+        All revved up and ready to go? Hit the button below to pay with Paypal!
+      </p>
+      <a class="btn-large">Register</a>
+    </div>
+  </div>
+</center></body>
+</html>
