67f2776a275d224b340a88f8195bb0a3b977225e
LunarGem <LunarGem@users.noreply.github.com>
2018-01-15 13:21:01 -0500

    Add CDNs

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <title>
         Les Cher Ballet
     </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 
   <body><center>
     <div class="container">
