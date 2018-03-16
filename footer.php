aa0d04b6313e662e233c6f61b0f1e9683339bb44
LunarGem <l.battle194@gmail.com>
2018-03-16 17:44:13 -0400

    Add header and footer

--- footer.php
+++ footer.php
@@ -1,1 +1,8 @@
-//Contact, address, etc.
+  <footer>
+  Copyright &copy; 2018 Les Cher Ballet <br/>
+  Les Cher Classical Ballet Academy<br/>
+  16903 Queen Anne Road<br/>
+  Upper Marlboro, MD 20774  <br/>
+  301-801-2946
+  240-463-5665
+</footer>
--- header.php
+++ header.php
@@ -1,1 +1,12 @@
-//logo, menu, etc.
+<div class="row">
+  <div class="col m8 s12 offset-m2">
+    <h1>Les Cher Ballet</h1>
+    <p>Your tagline here</p>
+  </div>
+  <div class="col m8 s12 offset-m2">
+    <a class="btn" href="index.php">Home</a>
+    <a class="btn" href="#">About</a>
+    <a class="btn" href="schedule.php">Classes</a>
+    <a class="btn" href="#">Showcase</a>
+  </div>
+</div>
