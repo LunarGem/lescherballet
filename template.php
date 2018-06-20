13542aa8ac3ac44f88a7b80e8f1e0b236d14fb1b
LunarGem <l.battle194@gmail.com>
2018-06-20 10:30:57 -0400

    add html template cause this style is hard to implement

---
+++ template.php
@@ -0,0 +1,33 @@
+<?php require('connect.php'); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <title>
+        Les Cher Ballet
+    </title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link type="text/css" rel="stylesheet" href="main.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <script>
+      function dynamo(){
+        $('.content').height($('.container').height());
+        $('.content').width($('.container').width());
+      }
+    </script>
+
+    <body onresize="javascript:dynamo()" onload="javascript:dynamo();">
+      <?php include('cornerbar.php'); ?>
+    <center>
+    <div class="container">
+      <div class="content"></div>
+      <?php include('header.php'); ?>
+      <br/>
+      <!--new content here -->
+      <div class="row">
+        <?php include('footer.php'); ?>
+      </div>
+    </div>
+  </center></body>
+</html>
