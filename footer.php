d95d7da5509a6fe8253e530e75b62ae5adffa23b
LunarGem <l.battle194@gmail.com>
2018-07-03 12:27:47 -0400

    add images to footer

--- footer.php
+++ footer.php
@@ -1,9 +1,17 @@
-  <footer>
-  Copyright &copy; 2018 Les Cher Ballet <br/>
-  Les Cher Classical Ballet Academy<br/>
-  16903 Queen Anne Road<br/>
-  Upper Marlboro, MD 20774  <br/>
-  301-801-2946
-  240-463-5665
+<footer style="font-family: Quattrocento Sans;">
+  <div class="col s12 m4">
+    <img src="images/logo.jpg"/>
+  </div>
+  <div class="col s12 m4">
+    Copyright &copy; 2018 Les Cher Ballet <br/>
+    Les Cher Classical Ballet Academy<br/>
+    16903 Queen Anne Road<br/>
+    Upper Marlboro, MD 20774  <br/>
+    301-801-2946 &nbsp;&nbsp;<span style="font-size: 25px;">&bull;</span> &nbsp;&nbsp; 240-463-5665<br/>
+    &nbsp;&nbsp;&nbsp;&nbsp;Kelly &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Elizabeth
+  </div>
+  <div class="col s12 m4">
+    <img src="images/dancer.jpg"/>
+  </div>
 </footer>
 <?php // TODO: make editable ?>
